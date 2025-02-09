72a9a20ee2efecda842ec4731592743e953bbf08
addcx1developer <addcx1developer@gmail.com>
2025-02-09 14:38:11 +0800

    fix: undefined mongodb uri from env

--- src/config/mongodb.config.ts
+++ src/config/mongodb.config.ts
@@ -1,3 +1,7 @@
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
+
 export const mongodbConfig = {
   uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/ph-barangay-db',
 };
